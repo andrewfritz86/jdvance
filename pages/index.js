74c3f4c91226959e6a2a6e53b8685f58df838c4a
Andrew Fritz <andrewfritz86@users.noreply.github.com>
2024-07-15 21:34:50 +0000

    quotes

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,15 @@ function Home() {
 
   return (
     <main className={styles.main}>
-      <h1> Fuck J.D. Vance</h1>
+      <a href='https://joebiden.com/'> Fuck JD Vance</a>
+      <ul>
+        <li>"I’m a Never Trump guy" - J.D. Vance </li>
+        <li> "My God, what an idiot - J.D. Vance </li>
+        <li> "I'm going to vote third party because I can't stomach Trump. I think that he's noxious and is leading the white working class to a very dark place." - J.D. Vance</li>
+        <li> "At the end of the day, do you believe Donald Trump, who always tells the truth - just kidding, or do you believe that woman on the tape." - J.D. Vance</li>
+        <li> "I can't stand Trump because I think he's a fraud, I think he's a total fraud who is exploiting these people." - J.D. Vance</li>
+        <li> "If I feel like Trump has a really good chance of winning, then I might have to hold my nose and vote for Hillary Clinton." - J.D. Vance</li>
+      </ul>
     </main>
   )
 }
