f4dbe89309da0575b4536629e7fc14c1e10bfd9f
Andrew Fritz <andrewfritz86@users.noreply.github.com>
2024-07-15 22:01:53 +0000

    analytic

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
-
 function throwError() {
   console.log(
     // The function body() is not defined
